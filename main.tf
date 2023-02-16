2457ab1017788ce0cabf367a9d13a0f915b3e092
Sacha Wharton <extern_wharton_sacha@allianz.de>
2023-02-16 20:00:57 +0200

    👌 IMPROVE: devspace img update

--- main.tf
+++ main.tf
@@ -64,6 +64,42 @@ resource "helm_release" "ortelius" {
   }
 }
 
+
+# ONLY ENABLE THIS IF YOU HAVE A LOCALSTACK PRO API KEY
+resource "kubectl_manifest" "localstack_apikey" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: localstack-apikey
+  namespace: localstack
+type: Opaque
+data:
+  localstack-apikey: ${base64encode(var.localstack_api_key)}
+YAML
+}
+
+# localstack https://docs.localstack.cloud/overview/
+# localstack helm charts https://github.com/localstack/helm-charts
+resource "helm_release" "localstack" {
+  name             = "localstack"
+  chart            = "localstack"
+  repository       = "https://helm.localstack.cloud"
+  namespace        = var.localstack_namespace
+  create_namespace = true
+  recreate_pods    = true
+  depends_on       = [kind_cluster.ortelius]
+  timeout          = 900
+  # ONLY ENABLE THIS IF YOU HAVE A LOCALSTACK PRO API KEY
+  values           = [file("localstack.yaml")]
+}
+# creates an S3 bucket called ortelius
+# accessible at http://s3.local.gd
+resource "aws_s3_bucket" "ortelius_bucket" {
+  bucket     = "ortelius-bucket"
+  depends_on = [helm_release.localstack]
+}
+
 # ortelius postgresql
 # https://artifacthub.io/packages/helm/bitnami/postgresql
 resource "helm_release" "postgresql" {
@@ -93,39 +129,4 @@ resource "helm_release" "postgresql" {
     name = "primary.service.nodePorts.postgresql"
     value = "35432"
   }
-}
-
-# ONLY ENABLE THIS IF YOU HAVE A LOCALSTACK PRO API KEY
-# resource "kubectl_manifest" "localstack_apikey" {
-#   yaml_body = <<YAML
-# apiVersion: v1
-# kind: Secret
-# metadata:
-#   name: localstack-apikey
-#   namespace: localstack
-# type: Opaque
-# data:
-#   localstack-apikey: ${base64encode(var.localstack_api_key)}
-# YAML
-# }
-
-# localstack https://docs.localstack.cloud/overview/
-# localstack helm charts https://github.com/localstack/helm-charts
-resource "helm_release" "localstack" {
-  name             = "localstack"
-  chart            = "localstack"
-  repository       = "https://helm.localstack.cloud"
-  namespace        = var.localstack_namespace
-  create_namespace = true
-  recreate_pods    = true
-  depends_on       = [kind_cluster.ortelius]
-  timeout          = 900
-  # ONLY ENABLE THIS IF YOU HAVE A LOCALSTACK PRO API KEY
-  #values           = [file("localstack.yaml")]
-}
-# creates an S3 bucket called ortelius
-# accessible at http://s3.local.gd
-resource "aws_s3_bucket" "ortelius_bucket" {
-  bucket     = "ortelius-bucket"
-  depends_on = [helm_release.localstack]
 }
