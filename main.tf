168160cae7c51241c3ad6122c8fc951c015eee35
Sacha Wharton <extern_wharton_sacha@allianz.de>
2023-02-20 15:47:05 +0200

    👌 IMPROVE: fix failed to create kubernetes rest client for read of resource: Get "http://localhost/api?timeout=32s": dial tcp [::1]:80: connect: connection refused

--- main.tf
+++ main.tf
@@ -21,13 +21,19 @@ resource "kind_cluster" "ortelius" {
       ]
       # ortelius http port
       extra_port_mappings {
+        container_port = 80
+        host_port      = 80
+        listen_address = "0.0.0.0"
+      }
+      # ortelius http port
+      extra_port_mappings {
         container_port = 30000
         host_port      = 8080
         listen_address = "0.0.0.0"
       }
       # postgresql port
       extra_port_mappings {
-        container_port = 31432
+        container_port = 30001
         host_port      = 5432
         listen_address = "0.0.0.0"
       }
@@ -46,19 +52,19 @@ resource "kind_cluster" "ortelius" {
 
 # ortelius postgresql
 # https://artifacthub.io/packages/helm/bitnami/postgresql
-resource "helm_release" "postgresql" {
-  name              = "postgresql"
-  chart             = "postgresql"
-  repository        = "https://charts.bitnami.com/bitnami"
-  namespace         = var.ortelius_namespace
-  create_namespace  = true
-  recreate_pods     = true
-  depends_on        = [kind_cluster.ortelius]
-  timeout           = 900
-  dependency_update = true
-  replace           = true
-  values = [file("values-postgresql.yaml")]
-}
+#resource "helm_release" "postgresql" {
+#  name              = "postgresql"
+#  chart             = "postgresql"
+#  repository        = "https://charts.bitnami.com/bitnami"
+#  namespace         = var.ortelius_namespace
+#  create_namespace  = true
+#  recreate_pods     = true
+#  depends_on        = [kind_cluster.ortelius]
+#  timeout           = 900
+#  dependency_update = true
+#  replace           = true
+#  values = [file("values-postgresql.yaml")]
+#}
 
 # ortelius
 # https://artifacthub.io/packages/helm/ortelius/ortelius
@@ -67,8 +73,10 @@ resource "helm_release" "ortelius" {
   chart             = "ortelius"
   repository        = "https://ortelius.github.io/ortelius-charts/"
   namespace         = var.ortelius_namespace
+  create_namespace  = true
   recreate_pods     = true
-  depends_on        = [helm_release.postgresql]
+  #depends_on        = [helm_release.postgresql]
+  depends_on        = [kind_cluster.ortelius]
   timeout           = 900
   dependency_update = true
   replace           = true
@@ -76,6 +84,10 @@ resource "helm_release" "ortelius" {
   set {
     name  = "ms-nginx.ingress.nodePort"
     value = "30000"
+  }
+    set {
+    name  = "ms-postgres.ingress.nodePort"
+    value = "30001"
   }
   # postgres password
   set {
@@ -87,7 +99,7 @@ resource "helm_release" "ortelius" {
     name = "ms-nginx.ingress.type"
     value = "ssloff"
   }
-  # wildcard ingress
+  # wildcard hostname for the ingress e.g. http://localhost:8080/dmadminweb/Home#dhmain
   set {
     name = "ms-nginx.ingress.dnsname"
     value = ""
@@ -101,36 +113,37 @@ resource "helm_release" "ortelius" {
 }
 
 # ONLY ENABLE THIS IF YOU HAVE A LOCALSTACK PRO API KEY
-# resource "kubectl_manifest" "localstack_apikey" {
-#   yaml_body = <<YAML
-# apiVersion: v1
-# kind: Secret
-# metadata:
-#   name: localstack-apikey
-#   namespace: localstack
-# type: Opaque
-# data:
-#   localstack-apikey: ${base64encode(var.localstack_api_key)}
-# YAML
-# }
+resource "kubectl_manifest" "localstack_apikey" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: localstack-apikey
+  namespace: localstack
+type: Opaque
+data:
+  localstack-apikey: ${base64encode(var.localstack_api_key)}
+YAML
+}
 
 # localstack https://docs.localstack.cloud/overview/
 # localstack helm charts https://github.com/localstack/helm-charts
-# resource "helm_release" "localstack" {
-#   name             = "localstack"
-#   chart            = "localstack"
-#   repository       = "https://helm.localstack.cloud"
-#   namespace        = var.localstack_namespace
-#   create_namespace = true
-#   recreate_pods    = true
-#   depends_on       = [kind_cluster.ortelius]
-#   timeout          = 900
-#   # ONLY ENABLE THIS IF YOU HAVE A LOCALSTACK PRO API KEY
-#   values = [file("localstack.yaml")]
-#}
+resource "helm_release" "localstack" {
+  name             = "localstack"
+  chart            = "localstack"
+  repository       = "https://helm.localstack.cloud"
+  #namespace        = var.localstack_namespace
+  namespace         = var.ortelius_namespace
+  # create_namespace = true
+  recreate_pods    = true
+  depends_on       = [helm_release.ortelius]
+  timeout          = 900
+  # ONLY ENABLE THIS IF YOU HAVE A LOCALSTACK PRO API KEY
+  values = [file("localstack.yaml")]
+}
 # creates an S3 bucket called ortelius
 # accessible at http://s3.local.gd
-# resource "aws_s3_bucket" "ortelius_bucket" {
-#   bucket     = "ortelius-bucket"
-#   depends_on = [helm_release.localstack]
-# }
+resource "aws_s3_bucket" "ortelius_bucket" {
+  bucket     = "ortelius-bucket"
+  depends_on = [helm_release.ortelius]
+}
